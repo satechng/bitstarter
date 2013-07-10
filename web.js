ea573c846eb7bc5f42e4b4d67f115ba3f728cefb
Saidi Adegoke <rasheedsaidi@gmail.com>
2013-07-10 22:25:23 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var inputfile = "index.html";
+var buf = fs.readFileSync(inputfile);
+var out = buf.toString("utf-8", 0, 27);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
